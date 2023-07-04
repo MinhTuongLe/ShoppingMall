7690cc2b6817145bf69c04cdf56827a1ac1a9620
LeMinhTuong <leminhtuong09122002@gmail.com>
2023-07-05 03:06:45 +0700

    new commit

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -6,7 +6,7 @@ import Loader from "../Loader/Loader";
 import Error from "../Error/Error";
 import "./ProductList.scss";
 import { useDispatch, useSelector } from "react-redux";
-import {formatCurrency} from "../../utils/formatCurrency"
+import { formatCurrency } from "../../utils/formatCurrency";
 
 const ProductList = ({ products, status }) => {
   const navigate = useNavigate();
@@ -15,7 +15,17 @@ const ProductList = ({ products, status }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [sort, setSort] = useState("latest");
   const [sortedProducts, setSortedProducts] = useState([]);
-  
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(Infinity);
+
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const filtered = sortedProducts.filter(
+      (product) => product.price >= minPrice && product.price <= maxPrice
+    );
+    setFilteredProducts(filtered);
+  }, [minPrice, maxPrice, sortedProducts]);
 
   useEffect(() => {
     if (sort === "latest") {
@@ -35,6 +45,13 @@ const ProductList = ({ products, status }) => {
     }
   }, [sort, products]);
 
+  useEffect(() => {
+    const maxProductPrice = Math.max(
+      ...products.map((product) => product.price)
+    );
+    setMaxPrice(maxProductPrice);
+  }, [products]);
+
   let productsPerPage = 10;
   let isHomePage = true;
   if (currentURL.includes("/category") || currentURL.includes("/products")) {
@@ -44,12 +61,12 @@ const ProductList = ({ products, status }) => {
 
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const currentProducts = sortedProducts.slice(
+  const currentProducts = filteredProducts.slice(
     indexOfFirstProduct,
     indexOfLastProduct
   );
 
-  const totalPages = Math.ceil(products.length / productsPerPage);
+  const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
   const isFirstPage = currentPage === 1;
   const isLastPage = currentPage === totalPages;
 
@@ -84,6 +101,11 @@ const ProductList = ({ products, status }) => {
     setCurrentPage(page);
   };
 
+  const handleClearFilter = () => {
+    setMinPrice(0)
+    setMaxPrice(1000)
+  }
+
   const noProductFound = <h4 className="empty-products">No products found!</h4>;
 
   return (
@@ -96,20 +118,34 @@ const ProductList = ({ products, status }) => {
             <h1 className="title">{products[0] && "Our Products"}</h1>
             {!isHomePage && (
               <>
-                {/* <div>
-                  <h4>Price Range</h4>
-                  <p>{formatCurrency(price)}</p>
-                  <div>
-                    <input
-                      type="range"
-                      step={formatCurrency(1000)}
-                      value={formatCurrency(price)}
-                      onChange={(e) => setPrice(e.target.value)}
-                      min={min_Price}
-                      max={max_Price}
-                    />
+                <div className="price-range-section">
+                  <label>Price Range:</label>
+                  <div className="filter-section">
+                    <div className="filter-input-section">
+                      {formatCurrency(minPrice)}
+                      <input
+                        style={{ marginLeft: "12px" }}
+                        type="range"
+                        value={minPrice}
+                        min={0}
+                        max={1000}
+                        onChange={(e) => setMinPrice(Number(e.target.value))}
+                      />
+                    </div>
+                    <div className="filter-input-section">
+                      {formatCurrency(maxPrice)}
+                      <input
+                        style={{ marginLeft: "12px" }}
+                        type="range"
+                        value={maxPrice}
+                        min={0}
+                        max={1000}
+                        onChange={(e) => setMaxPrice(Number(e.target.value))}
+                      />
+                    </div>
                   </div>
-                </div> */}
+                  <Button style={{marginLeft:"18px"}} onClick={handleClearFilter}>Clear Filter</Button>
+                </div>
                 <div className="sort-section">
                   <label>Sort by:</label>
                   <select
